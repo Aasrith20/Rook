7e21a2bc147105b8b8c4955e3ba20b53b2953d16
Aasrith20 <62925727+Aasrith20@users.noreply.github.com>
2020-09-16 14:56:33 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,19 @@
+function Time(){
+    var hours = document.getElementById("first_box");
+    var minutes = document.getElementById("second_box");
+    var a = new Date();
+
+    if (a.getHours() > 12) {
+        hours.innerHTML = a.getHours() - 12;
+    }
+    else {
+        hours.innerHTML = a.getHours();
+    }
+
+    minutes.innerHTML = a.getMinutes();
+    console.log(a.getMinutes());
+}
+function Initiate(){
+    Time();
+    setInterval(Time,1000);
+}
